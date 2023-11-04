c1978e7ba025db45737980f37d9556180e649df0
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2023-11-04 23:20:35 +0200

    Implemented forward-Renderer
    Implemented forward-Renderer and fixed bug in entity-test-state

--- source/common/systems/forward-renderer.cpp
+++ source/common/systems/forward-renderer.cpp
@@ -2,6 +2,9 @@
 #include "../mesh/mesh-utils.hpp"
 #include "../texture/texture-utils.hpp"
 
+#define viewPortStart_X 0
+#define viewPortStart_Y 0
+
 namespace our {
 
     void ForwardRenderer::initialize(glm::ivec2 windowSize, const nlohmann::json& config){
@@ -135,21 +138,28 @@ namespace our {
 
         //TODO: (Req 9) Modify the following line such that "cameraForward" contains a vector pointing the camera forward direction
         // HINT: See how you wrote the CameraComponent::getViewMatrix, it should help you solve this one
-        glm::vec3 cameraForward = glm::vec3(0.0, 0.0, -1.0f);
+        //camera forward is the third row of the view matrix
+        glm::vec3 cameraForward = glm::normalize(glm::vec3(camera->getViewMatrix()[2]));
         std::sort(transparentCommands.begin(), transparentCommands.end(), [cameraForward](const RenderCommand& first, const RenderCommand& second){
             //TODO: (Req 9) Finish this function
-            // HINT: the following return should return true "first" should be drawn before "second". 
-            return false;
+            // HINT: the following return should return true "first" should be drawn before "second".
+            //calculating the distance between the camera and the center of the objects
+            float distanceToFirst = glm::distance(first.center, cameraForward);
+            float distanceToSecond = glm::distance(second.center, cameraForward);
+              // The command with the greater distance should be drawn first
+            return distanceToFirst > distanceToSecond;
         });
 
         //TODO: (Req 9) Get the camera ViewProjection matrix and store it in VP
-        
+        glm::mat4 VP = camera->getProjectionMatrix(windowSize) * camera->getViewMatrix();
         //TODO: (Req 9) Set the OpenGL viewport using viewportStart and viewportSize
-        
+        glViewport(viewPortStart_X, viewPortStart_Y, windowSize.x, windowSize.y);
         //TODO: (Req 9) Set the clear color to black and the clear depth to 1
-        
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearDepth(1.0f);
         //TODO: (Req 9) Set the color mask to true and the depth mask to true (to ensure the glClear will affect the framebuffer)
-        
+        glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+        glDepthMask(GL_TRUE);
 
         // If there is a postprocess material, bind the framebuffer
         if(postprocessMaterial){
@@ -158,10 +168,16 @@ namespace our {
         }
 
         //TODO: (Req 9) Clear the color and depth buffers
-        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         //TODO: (Req 9) Draw all the opaque commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for (auto &command : opaqueCommands){
+            //Set the "transform" uniform to be equal the model-view-projection matrix for each render command
+            glm::mat4 MVP = VP * command.localToWorld;
+            command.material->setup();
+            command.material->shader->set("transform", MVP);
+            command.mesh->draw();
+        }
         // If there is a sky material, draw the sky
         if(this->skyMaterial){
             //TODO: (Req 10) setup the sky material
@@ -185,7 +201,13 @@ namespace our {
         }
         //TODO: (Req 9) Draw all the transparent commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for (auto &command : transparentCommands){
+            // Set the "transform" uniform to be equal the model-view-projection matrix for each render command
+            glm::mat4 MVP = VP * command.localToWorld;
+            command.material->setup();
+            command.material->shader->set("transform", MVP);
+            command.mesh->draw();
+        }
 
         // If there is a postprocess material, apply postprocessing
         if(postprocessMaterial){
